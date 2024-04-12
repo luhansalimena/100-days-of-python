46c9e6bff8a095302f35701c4efca9a1ba83fa40
Luhan Salimena <luhan@beepayapp.com.br>
2024-04-11 22:46:04 -0300

    Add Lesson 7 Day 2 code

---
+++ day-2-data-type/LESSON 7 DAY 2 - LIFE IN WEEKS/lesson7.py
@@ -0,0 +1,8 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+totalWeeks = 90 * 52
+yourWeeks = int(age) * 52
+remainingWeeks = totalWeeks - yourWeeks
+print(f"You have {remainingWeeks} weeks left.")
